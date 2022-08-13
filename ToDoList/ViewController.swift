ad48c10c90348f1626a1ab9cf1086a4e8bdf79ad
İsa Kulaksız <isakulaksiz@outlook.com>
2022-08-13 13:02:00 +0300

    Initial Commit

---
+++ ToDoList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by Isa Kulaksız on 13.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
