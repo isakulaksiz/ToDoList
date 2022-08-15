e8623443226795c5d84d1f380bce91fff3f4e772
İsa Kulaksız <isakulaksiz@outlook.com>
2022-08-15 20:13:08 +0300

    added task detail

--- ToDoList/TaskViewController.swift
+++ ToDoList/TaskViewController.swift
@@ -9,10 +9,23 @@ import UIKit
 
 class TaskViewController: UIViewController {
 
+    @IBOutlet var label: UILabel!
+    
+    var tasks: String?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        label.text = tasks
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Delete", style: .done, target: self, action: #selector(deleteTask))
+    }
+    
+    @objc func deleteTask(){
+        //let newCount = count - 1
+        //UserDefaults().setValue(newCount, forKey: "count")
+        //UserDefaults().setValue(nil, forKey: "task_\(currentPosition)")
+        
     }
 
 }
--- ToDoList/ViewController.swift
+++ ToDoList/ViewController.swift
@@ -62,7 +62,15 @@ extension ViewController: UITableViewDelegate{
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
+        let vc = storyboard?.instantiateViewController(identifier: "task") as! TaskViewController
+        vc.title = "New Task"
+        vc.tasks = tasks[indexPath.row]
+        //vc.task = tasks[indexPath.row]
+        navigationController?.pushViewController(vc, animated: true)
     }
+    
+   
+    
 }
 
 extension ViewController: UITableViewDataSource{
